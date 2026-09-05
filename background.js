e4152429357cdf7b01cd2e3d952ab9119fcbc908
Redeye424 <shawngoldberg424@gmail.com>
2026-09-05 18:51:51 -0400

    the blocker

---
+++ background.js
@@ -0,0 +1,31 @@
+let ShortWatchedInARow = 0;
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (!changeInfo.url) {
+        return;
+    }
+
+    const url = new URL(changeInfo.url);
+
+    const isYouTubeDomain =
+        url.hostname === 'youtube.com' ||
+        url.hostname === 'www.youtube.com';
+
+    if (!isYouTubeDomain) {
+        return;
+    }
+
+    if (url.pathname.startsWith('/shorts/')) {
+        ShortWatchedInARow++;
+
+        console.log("Shorts watched:", ShortWatchedInARow);
+    }
+
+    if (ShortWatchedInARow >= 3) {
+        ShortWatchedInARow = 0;
+
+        chrome.tabs.update(tabId, {
+            url: 'https://www.youtube.com/shorts/A_LXWYHAbs0'
+        });
+    }
+});
