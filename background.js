83f3c943b9dd209d24395999418f7401cec90027
Redeye424 <shawngoldberg424@gmail.com>
2026-09-15 16:20:31 -0400

    Revert "Update background.js"
    This reverts commit 1cbe9610b07f63797cd39419a8eba83d196c7682.

--- background.js
+++ background.js
@@ -1,5 +1,4 @@
 let ShortWatchedInARow = 0;
-let LastShortURL = "";
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     if (!changeInfo.url) {
@@ -15,39 +14,24 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     }
 
     const isYouTubeDomain =
-        url.hostname === "youtube.com" ||
-        url.hostname === "www.youtube.com";
+        url.hostname === 'youtube.com' ||
+        url.hostname === 'www.youtube.com';
 
     if (!isYouTubeDomain) {
         return;
     }
 
-    // User entered a YouTube Short
-    if (url.pathname.startsWith("/shorts/")) {
-
-        // Don't count the same Short multiple times
-        if (changeInfo.url === LastShortURL) {
-            return;
-        }
-
-        LastShortURL = changeInfo.url;
+    if (url.pathname.startsWith('/shorts/')) {
         ShortWatchedInARow++;
-
-        console.log("Shorts watched in a row:", ShortWatchedInARow);
-
-        // After 2 Shorts
-        if (ShortWatchedInARow >= 2) {
-            ShortWatchedInARow = 0;
-            LastShortURL = "";
-
-            chrome.tabs.update(tabId, {
-                url: "https://homework42.pythonanywhere.com/Youtube_Shorts_Blocker"
-            });
-        }
-
     } else {
-        // They left Shorts, so reset the streak
         ShortWatchedInARow = 0;
-        LastShortURL = "";
+    }
+
+    if (ShortWatchedInARow >= 2) {
+        ShortWatchedInARow = 0;
+
+        chrome.tabs.update(tabId, {
+            url: 'https://homework42.pythonanywhere.com/Youtube_Shorts_Blocker'
+        });
     }
 });
